ec3aa7a83c1717781c6ca4c884e64ae5bf233c0e
ahmed.abozaid1 <ahmed.abozaid128@gmail.com>
2022-05-23 12:37:20 +0200

    fix string issue

--- RSAAlgorithm/Program.cs
+++ RSAAlgorithm/Program.cs
@@ -5,8 +5,39 @@ using System.Text;
 namespace RSAAlgorithm;
 class RSA
 {
-    public List<BigInteger> Tochunks(string s,BigInteger kn)
+    public List<BigInteger> Tochunks2(string s, BigInteger kn)
     {
+        List<BigInteger> list = new List<BigInteger>();
+        string k = "";
+        string temp = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            temp = k;
+            temp += s[i];
+           
+            if (BigInteger.Parse(temp) > kn)
+            {
+                list.Add(BigInteger.Parse(k));
+                k = "";
+                k += s[i];
+            }
+            else
+            {
+                k += s[i];
+                if(i == s.Length-1)
+                {
+                    list.Add(BigInteger.Parse(k));
+                }
+            }
+        }
+
+
+
+            return list;
+    }
+
+   public List<BigInteger> Tochunks(string s,BigInteger kn)
+   {
         List<BigInteger> list = new List<BigInteger>();
         string k = "";
         string temp = "";
@@ -18,12 +49,20 @@ class RSA
             if (n > kn)
             {
                 n = BigInteger.Parse(k);
+                if (k[0] == '0')
+                {
+                    Console.WriteLine("ahhhhhhh");
+                }
                 list.Add(n);
                 k = "";
             }
             else
             {
                 k += s[i];
+                if (k[0] == '0')
+                {
+                    Console.WriteLine("ahhhhhhh");
+                }
                 if (i == s.Length-1)
                 {
                     n = BigInteger.Parse(k);
@@ -32,7 +71,7 @@ class RSA
             }
         }
         return list;
-    } 
+   } 
 
     public string stringInput(string s)
     {
@@ -56,46 +95,81 @@ class RSA
         RSA rsa = new RSA();
         Console.WriteLine("here");
         Console.WriteLine(key.p);
-       // string s = rsa.stringInput("Ahmed Hamada Mohamed Aboziad /// Adham Khaled Hassan Ahmed");
-
+         string ins = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        //
+        //string ins = "ahmed hamada mohaemd abozaid /// /// / // / / Adhma khaled hasaan ahmed";
+        //string ins = "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadasdasdasdasdasdasdasdasdasasdasdasdasdasdasdasdasdasdasdasdasdsadadsaasasasdasdasdasdasdasdasdasd";
+        byte[] byt = Encoding.ASCII.GetBytes(ins);
+        BigInteger number = new BigInteger(byt);
         
-        RandomNumberGenerator rng = RandomNumberGenerator.Create();
-        byte[] bytes = new byte[256];
-        BigInteger a;
-        rng.GetBytes(bytes);
-        a = new BigInteger(bytes);
-        a = BigInteger.Abs(a);
-        string s = rsa.numberInput(a);
+        string s = rsa.stringInput(ins);
+ 
+
+      //  RandomNumberGenerator rng = RandomNumberGenerator.Create();
+      // byte[] bytes = new byte[256];
+      //  BigInteger a;
+      //   rng.GetBytes(bytes);
+      //   a = new BigInteger(bytes);
+      //   a = BigInteger.Abs(a);
+      //     string s = rsa.numberInput(a);
         Console.WriteLine("input");
-        Console.WriteLine(a);
+    //    Console.WriteLine(a);
         Console.WriteLine("string");
         s.ToString();
         Console.WriteLine(s);
        
-        List<BigInteger> list = rsa.Tochunks(s,key.n);
+        List<BigInteger> list = rsa.Tochunks2(s,key.n);
         Console.WriteLine("list");
+        string check ="";
         for (int i = 0; i < list.Count; i++)
         {
+            check+=list[i].ToString();
             Console.WriteLine(list[i]);
         }
        
+        if (BigInteger.Parse(check) == number)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("similar to check");
+            Console.WriteLine(check);
+            Console.WriteLine("");
+            Console.WriteLine("simi");
+        }
+        else
+        {
+            Console.WriteLine("wrong");
+        }
         EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
         List<BigInteger> cypher = encryptDecrypt.Encrypt(list, key.e, key.n);
          
         List<BigInteger> plain = encryptDecrypt.Decrypt(cypher, key.d, key.n);
         Console.WriteLine("decrypted");
       
-           for (int i = 0; i < plain.Count; i++)
-           {
-               Console.WriteLine(plain[i]);
-           }
-           s = "";
-         // for (int i = 0; i < plain.Count; i++)
+        //   for (int i = 0; i < plain.Count; i++)
         //   {
-        //         s += Encoding.ASCII.GetString(plain[i].ToByteArray());
-       //   }
-       //   Console.WriteLine(s);
-
+       //        Console.WriteLine(plain[i]);
+      //     }
+           string o = "";
+        
+        for (int i = 0; i < plain.Count; i++)
+         {
+  
+            o += plain[i].ToString();   
+         }
+        byte[] bytes = BigInteger.Parse(o).ToByteArray();
+        ASCIIEncoding ascii = new ASCIIEncoding();
+        o = ascii.GetString(bytes);
+        Console.WriteLine(o);
+        Console.WriteLine(" ");
+        if (o== ins)
+        {
+            
+            Console.WriteLine("similar");
+        }
+        else
+        {
+            Console.WriteLine("different");
+        }
 
 
 
